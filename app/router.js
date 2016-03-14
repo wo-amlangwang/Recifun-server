00e8002477582495ef505f0a17e3fac92f6a5a38
wo-amlangwang <adamlangwang@gmail.com>
2016-03-13 23:06:41 -0700

    commont unused part

--- app/router.js
+++ app/router.js
@@ -16,18 +16,18 @@ module.exports = function(app,passport) {
     req.logout();
     res.sendStatus(200);
   });
-
+/*
   var form = "<!DOCTYPE HTML><html><body>" +
 "<form method='post' action='/upload' enctype='multipart/form-data'>" +
 "<input type='file' name='image'/>" +
 "<input type='submit' /></form>" +
-"</body></html>";
+"</body></html>";*/
 
   app.get('/',function(req,res,next) {
-    res.writeHead(200, {'Content-Type': 'text/html' });
-    res.end(form);
+    //res.writeHead(200, {'Content-Type': 'text/html' });
+    //res.end(form);
   });
-  app.post('/upload',upload.single('image'),function (req,res) {
+  app.post('/api/pictureUpload',upload.single('image'),function (req,res) {
     res.sendStatus(200);
   });
 
